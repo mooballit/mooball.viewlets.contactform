f3c56324b53012ec68927aa9f48c914865624793
Roman Joost <roman@mooball.com>
2011-12-20 09:52:32 +1000

    Bootstrapping contact form viewlet.

---
+++ README.txt
@@ -0,0 +1,6 @@
+.. contents::
+
+Introduction
+============
+A contact form in a viewlet, in case it needs to be deployed in a footer
+on a page.
---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+version = '0.1'
+tests_require = ['plone.app.testing']
+
+setup(name='mooball.viewlets.contactform',
+      version=version,
+      description="Contact Form viewlet",
+      long_description="",
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Roman Joost',
+      author_email='roman@mooball.net',
+      license='gpl',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['mooball'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'five.grok',
+      ],
+      extras_require=dict(tests=tests_require),
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins = ["ZopeSkel"],
+
+      )
